21eddd1e1e384f41d851097189cb00999eeacba1
andreeamiclaus <96205415+andreeamiclaus@users.noreply.github.com>
2022-09-17 16:32:14 +0200

    Update

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function showEmergencyAmount() {
     travel -
     others2;
 
-  const emergencyFunds = (fixedCosts / 100) * 70;
+  const emergencyFunds = (fixedCosts / 100) * 50;
   alert("You can save " + emergencyFunds + " euros for black days!");
 }
 
@@ -94,6 +94,33 @@ function showFunAmount() {
     travel -
     others2;
 
-  const funFunds = (fixedCosts / 100) * 30;
+  const funFunds = (fixedCosts / 100) * 20;
   alert("You can spend " + funFunds + " euros for fun!");
+}
+
+function showInvestmentAmount() {
+  const income = document.getElementById("monthly-income").value;
+  const livingCosts = document.getElementById("living-costs").value;
+  const food = document.getElementById("food").value;
+  const transportation = document.getElementById("transportation").value;
+  const pets = document.getElementById("pets").value;
+  const others = document.getElementById("others").value;
+  const hobbies = document.getElementById("hobbies").value;
+  const privateNeeds = document.getElementById("private-needs").value;
+  const travel = document.getElementById("travel").value;
+  const others2 = document.getElementById("others2").value;
+  const fixedCosts =
+    income -
+    livingCosts -
+    food -
+    transportation -
+    pets -
+    others -
+    hobbies -
+    privateNeeds -
+    travel -
+    others2;
+
+  const investmentFunds = (fixedCosts / 100) * 30;
+  alert("You can invest " + investmentFunds + " euros !");
 }
